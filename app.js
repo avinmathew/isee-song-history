327d286b9e3b66d73525e8642964bbe651167229
Avin Mathew <avinmathew@users.noreply.github.com>
2020-07-05 22:24:10 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,132 @@
+const spreadsheetUrl = 'https://spreadsheets.google.com/feeds/list/15sEuDqyqvvCVB2zs9A3BMhb4XtDvSM-BwZTJpRnNJVo/od6/public/values?alt=json'
+
+let performances;
+let songs;
+let selectedSong;
+
+// From https://stackoverflow.com/a/3177838
+function timeSince(date) {
+  const seconds = Math.floor((new Date() - date) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+
+function distinctSongs() {
+  songs = performances.map(s => `${s.song} (${s.version})`)
+                      .filter((value, index, self) => self.indexOf(value) === index)
+                      .sort();
+}
+
+function renderSongSelect() {
+  const $songSearch = $('#song-search').select2({
+    theme: 'bootstrap',
+    placeholder: {
+      id: '-1',
+      text: 'Select a song'
+    },
+    data: songs.map(s => ({id: s, text: s}))
+  }).on('select2:select', e => {
+    if (e.params && e.params.data.id) {
+      selectedSong = e.params.data.id;
+    }
+    if (selectedSong) {
+      renderSongTable();
+      renderSongTimeline();
+    }
+    return true;
+  });
+  $songSearch.show();
+  // As first option is already selected
+  $songSearch.trigger('select2:select');
+}
+
+function renderSongTable() {
+  const selected = performances.filter(p => `${p.song} (${p.version})` === selectedSong);
+  const grouped = selected.reduce((result, item) => {
+    if (!result[item.worshipLeader]) {
+      result[item.worshipLeader] = {};
+    }
+    if (!result[item.worshipLeader][item.key]) {
+      result[item.worshipLeader][item.key] = {
+        count: 0,
+        lastSung: item.date,
+      };
+    }
+    result[item.worshipLeader][item.key].count += 1;
+    if (result[item.worshipLeader][item.key].lastSung < item.date) {
+      result[item.worshipLeader][item.key].lastSung = item.date;
+    }
+    return result;
+  }, {});
+  const html = Object.keys(grouped).sort().map(wl => {
+    return Object.keys(grouped[wl]).sort().map(key => {
+      const item = grouped[wl][key];
+      const lastSung = `${timeSince(item.lastSung)} ago`;
+      return `<tr><td>${wl}</td><td>${key}</td><td>${item.count}</td><td>${lastSung}</td></tr>`;
+    });
+  }).join('');
+  $('#song-table-body').html(html);
+  $('#song-table').show();
+}
+
+function renderSongTimeline() {
+  const selected = performances.filter(p => `${p.song} (${p.version})` === selectedSong);
+  const dates = selected.map(s => s.date)
+    .filter(d => d > oneYearAgo);
+  $('#timeline-section').show();
+  let sungTimes;
+  if (dates.length === 1) {
+    sungTimes = 'Once';
+  } else if (dates.length === 2) {
+    sungTimes = 'Twice';
+  } else {
+    sungTimes = `${dates.length} times`;
+  }
+  sungTimes += ' in the past year';
+  $('#sung-times').html(sungTimes);
+  addCircles(dates);
+}
+
+fetch(spreadsheetUrl)
+  .then(response => {
+    if (response.ok) {
+      response.json().then(data => {
+        // Transform Google Sheets data structure
+        const rows = data.feed.entry.map(row => {
+          return {
+            date: new Date(row['gsx$date']['$t']),
+            song: row['gsx$song']['$t'],
+            version: row['gsx$version']['$t'],
+            worshipLeader: row['gsx$worshipleader']['$t'],
+            key: row['gsx$key']['$t']
+          }
+        });
+        performances = rows;
+        distinctSongs();
+        renderSongSelect();
+      });
+    } else {
+      console.error('Could not read data feed');
+    }
+  });
+
---
+++ timeline.js
@@ -0,0 +1,50 @@
+const margin = {top: 10, right: 40, bottom: 20, left: 10};
+const width = $('.container').outerWidth() - margin.left - margin.right;
+const height = 50 - margin.top - margin.bottom;
+
+const svg = d3.select('#timeline')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform',
+          'translate(' + margin.left + ',' + margin.top + ')');
+
+
+// Add X axis
+const oneYearAgo = new Date(new Date().getTime() - 365 * 24 * 60 * 60 * 1000);
+const x = d3.scaleTime()
+  .domain([oneYearAgo, new Date()])
+  .range([0, width])
+  .nice();
+svg.append('g')
+  .attr('class', 'axis')
+  .attr('transform', 'translate(0,' + height + ')')
+  .call(
+    d3.axisBottom(x)
+      .tickFormat(
+        d3.timeFormat('%b')
+      )
+  );
+
+const t = d3.transition().duration(1000);
+
+function addCircles(data) {
+  svg.selectAll('circle')
+    .data(data)
+    .join(
+      enter => enter.append('circle')
+        .attr('cx', date => x(date))
+        .attr('cy', () => height - 10)
+        .attr('r', 5)
+        .style('fill', '#DD0332')
+        .style('fill-opacity', 0)
+        .transition(t)
+        .style('fill-opacity', 1),
+      update => update.transition(t)
+        .attr('cx', date => x(date)),
+      exit => exit.transition(t)
+                  .style('fill-opacity', 0)
+                  .remove()
+    )
+}
